1f21dd531db3f09bfc391139e0924cf76d85addb
NadjanSF <nda-ph@hotmail.com>
2025-10-30 18:30:20 -0300

    Versão final do projeto App Hotel com tela Sobre

---
+++ MauiAppHotel-master/MauiAppHotel/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppHotel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        //linha adicionada para registrar a rota da nova página Sobre//
+            Routing.RegisterRoute(nameof(Sobre), typeof(Sobre));
+        }
+    }
+}
